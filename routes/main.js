b2a0f897f407e2b7adfd5d05695e6583a440b9f8
tflins <itan.flin@gmail.com>
2018-08-05 18:52:42 +0800

    实现首页文章的分类显示

--- routes/main.js
+++ routes/main.js
@@ -10,10 +10,21 @@ const router = express.Router();
 
 // 首页路由配置
 router.get("/", (req, res) => {
+    // 定义一个变量用来存放传递给模板的其他信息
+    let other = {};
+    // 分类查询条件
+    let where = {};
+    // 接收前端传递过来的需要查询分类的id
+    if (req.query.categoryId) {
+        other.categoryId = req.query.categoryId;
+        where.category = req.query.categoryId;
+    }
+
     // 从数据库中查询分类信息
     categoryModel.find({}, (err, categories) => {
         if (!err) {
             // 如果不出错
+            other.categories = categories;
             // 调用分页渲染模块渲染内容
             pagination({
                 // 每页显示的条数
@@ -25,13 +36,13 @@ router.get("/", (req, res) => {
                 // 渲染的模板页面
                 ejs: "main/index",
                 // 查询的条件
-                where: {},
+                where: where,
                 // 给模板绑定参数的名称
                 res: res,
                 req: req,
                 populate: ["category", "author"],
                 // 其他数据
-                other: categories
+                other: other
             });
         } else {
             throw err;
