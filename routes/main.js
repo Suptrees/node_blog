eb3840e22936fae4c7907365062560864d0dd8c8
tflins <itan.flin@gmail.com>
2018-08-08 19:17:34 +0800

    实现了文章内容的页面，及阅读量的增加

--- routes/main.js
+++ routes/main.js
@@ -8,45 +8,73 @@ const pagination = require("../my_modules/pagination");
 // 实例化Router对象
 const router = express.Router();
 
-// 首页路由配置
-router.get("/", (req, res) => {
-    // 定义一个变量用来存放传递给模板的其他信息
-    let other = {};
-    // 分类查询条件
-    let where = {};
+// 定义一个变量用来存放传递给模板的其他信息
+let other = {};
+// 分类查询条件
+let where = {};
+
+// 处理通用数据
+router.use("/", (req, res, next) => {
     // 接收前端传递过来的需要查询分类的id
     if (req.query.categoryId) {
         other.categoryId = req.query.categoryId;
         where.category = req.query.categoryId;
+    } else {
+        where = {};
+        other.categoryId = null;
     }
 
-    // 从数据库中查询分类信息
     categoryModel.find({}, (err, categories) => {
         if (!err) {
             // 如果不出错
             other.categories = categories;
-            // 调用分页渲染模块渲染内容
-            pagination({
-                // 每页显示的条数
-                limit: 10,
-                // 需要操作的数据库模型
-                model: contentModel,
-                // 需要控制分页的url
-                url: "/",
-                // 渲染的模板页面
-                ejs: "main/index",
-                // 查询的条件
-                where: where,
-                // 给模板绑定参数的名称
-                res: res,
-                req: req,
-                populate: ["category", "author"],
-                // 其他数据
-                other: other
-            });
         } else {
             throw err;
         }
+    });
+
+    next();
+});
+
+// 首页路由配置
+router.get("/", (req, res) => {
+
+        // 调用分页渲染模块渲染内容
+        pagination({
+            // 每页显示的条数
+            limit: 10,
+            // 需要操作的数据库模型
+            model: contentModel,
+            // 需要控制分页的url
+            url: "/",
+            // 渲染的模板页面
+            ejs: "main/index",
+            // 查询的条件
+            where: where,
+            // 给模板绑定参数的名称
+            res: res,
+            req: req,
+            populate: ["category", "author"],
+            // 其他数据
+            other: other
+        });
+});
+
+// 内容页面
+router.get("/views", (req, res) => {
+    // 获取文章id
+    let contentId = req.query.contentId;
+    // 根据id从数据库中查询文章内容
+    contentModel.findById(contentId).populate(["category", "author"]).then((content) => {
+        // 渲染内容模板
+        res.render("main/views", {
+            userInfo: req.userInfo,
+            other: other,
+            content: content
+        });
+        // 阅读量增加
+        content.views ++;
+        content.save();
     });
 });
 
